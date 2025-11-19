5464ff864a6d007be53f4a3b46feaeee9e8cc960
Hugo <hugo.payet@epitech.eu>
2025-11-20 04:14:09 +0900

    build(build.sh): adding Clang_DIR

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@ mkdir -p build && cd build
 
 cmake .. \
   -DLLVM_DIR=$(brew --prefix llvm)/lib/cmake/llvm \
+  -DClang_DIR=$(brew --prefix llvm)/lib/cmake/clang \
   -DCMAKE_BUILD_TYPE=Release \
 && make -j$(sysctl -n hw.logicalcpu)
