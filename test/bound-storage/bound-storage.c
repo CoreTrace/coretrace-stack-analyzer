a86ae0e760ba807680cfc339df3ade63e70da3c4
Hugo <hugo.payet@epitech.eu>
2025-11-20 17:03:19 +0900

    test(bound-storage): adding some test

---
+++ test/bound-storage/bound-storage.c
@@ -0,0 +1,20 @@
+int main(void)
+{
+    char test[10];
+
+    // [!!] potential stack buffer overflow on variable '<unnamed>' (size 10)
+    //     constant index 11 is out of bounds (0..9)
+    //     (this is a write access)
+    test[11] = 'a';
+
+    test[9] = 'b';  // OK
+
+    // [!!] potential stack buffer overflow on variable '<unnamed>' (size 10)
+    //     constant index 18446744073709551615 is out of bounds (0..9)
+    //     (this is a write access)
+    test[-1] = 'c';
+
+    test[11 - 2] = 'd';  // OK
+
+    return 0;
+}
