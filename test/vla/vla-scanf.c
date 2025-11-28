541773281a8115acb18b9288ad40a14f2f986da2
Hugo <hugo.payet@epitech.eu>
2025-11-29 07:02:15 +0900

    test: skip vla scanf test

--- test/vla/vla-scanf.c
+++ test/vla/vla-scanf.c
@@ -3,13 +3,13 @@
 int main(void)
 {
     int n;
-    // at line 9, column 9
+    // --- at line 9, column 9
     // [!!] stack pointer escape: address of variable 'n' escapes this function
     //     address passed as argument to function 'scanf' (callee may capture the pointer beyond this function)
     if (scanf("%d", &n) != 1)
         return 1;
 
-    // at line 16, column 5
+    // --- at line 16, column 5
     // [!] dynamic stack allocation detected for variable 'vla'
     //     allocated type: i8
     //     size of this allocation is not compile-time constant (VLA / variable alloca) and may lead to unbounded stack usage
