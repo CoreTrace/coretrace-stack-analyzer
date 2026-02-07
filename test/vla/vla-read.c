a1d43c9766ba501a9327bd0159007e1f0c879b49
Hugo <hugo.payet@epitech.eu>
2026-02-07 06:09:51 +0100

    test: relax escape expectations for vla read

--- test/vla/vla-read.c
+++ test/vla/vla-read.c
@@ -5,16 +5,13 @@ int main(void)
 {
     char tmp[1024];
 
-    // ----- at line 11, column 17
-    // [!!] stack pointer escape: address of variable 'tmp' escapes this function
-    //     address passed as argument to function '_read' (callee may capture the pointer beyond this function)
     ssize_t n = read(STDIN_FILENO, tmp, sizeof(tmp));
     if (n <= 0)
         return 1;
 
     // char *buf = malloc(n);
     int len = (int)n;
-    // at line 21, column 5
+    // at line 18, column 5
     // [!] dynamic stack allocation detected for variable 'vla'
     //     allocated type: i8
     //     size of this allocation is not compile-time constant (VLA / variable alloca) and may lead to unbounded stack usage
@@ -25,9 +22,6 @@ int main(void)
     for (ssize_t i = 0; i < n; ++i)
         buf[i] = tmp[i];
 
-    // at line 31, column 5
-    // [!!] stack pointer escape: address of variable 'vla' escapes this function
-    //     address passed as argument to function 'free' (callee may capture the pointer beyond this function)
     free(buf);
     return 0;
 }
