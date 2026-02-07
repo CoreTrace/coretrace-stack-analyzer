930fb80e2547f03a7f689a75b016807a356b8a85
Hugo <hugo.payet@epitech.eu>
2026-02-07 06:16:33 +0100

    test: add strncpy size-minus-one case

---
+++ test/size-arg/strncpy-size-minus-1.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include <stddef.h>
+
+void foo(char *dst, const char *src, size_t n)
+{
+    // at line 10, column 5
+    // [!] potential unsafe write with length (size - 1) in __strncpy_chk
+    //     destination pointer may be null
+    //     size operand may be <= 1
+    strncpy(dst, src, n - 1);
+}
+
+int main(void)
+{
+    char a[8] = {0};
+    char b[8] = {0};
+    foo(a, b, 8);
+    return 0;
+}
