e1603da149d38cb996894fa0ed22c75d62ab27d9
Hugo <hugo.payet@epitech.eu>
2026-02-09 02:41:21 +0100

    test(uninitialized): add interprocedural partial struct initialization case

---
+++ test/uninitialized-variable/uninitialized-local-interproc-struct-partial.c
@@ -0,0 +1,21 @@
+struct Pair
+{
+    int x;
+    int y;
+};
+
+static void init_x(struct Pair* p)
+{
+    p->x = 1;
+}
+
+int read_struct_partial_via_call(void)
+{
+    struct Pair p;
+    init_x(&p);
+    return p.y;
+}
+
+// at line 16, column 14
+// [!!] potential read of uninitialized local variable 'p'
+//      this load may execute before any definite initialization on all control-flow paths
