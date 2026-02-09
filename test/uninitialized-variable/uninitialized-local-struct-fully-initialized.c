425eed8db6b00f02fd03726aeab68af41721d04b
Hugo <hugo.payet@epitech.eu>
2026-02-09 02:41:21 +0100

    test(uninitialized): add fully initialized struct no-warning case

---
+++ test/uninitialized-variable/uninitialized-local-struct-fully-initialized.c
@@ -0,0 +1,15 @@
+struct Point
+{
+    int x;
+    int y;
+};
+
+int read_struct_fully_initialized(void)
+{
+    struct Point p;
+    p.x = 1;
+    p.y = 2;
+    return p.y;
+}
+
+// not contains: potential read of uninitialized local variable 'p'
