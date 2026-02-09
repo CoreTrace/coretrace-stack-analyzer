9801e8bd3053fd390bb1e2beac60a71e522d7e29
Hugo <hugo.payet@epitech.eu>
2026-02-09 03:13:03 +0100

    test(uninitialized): add regression for memcpy reading uninitialized source

---
+++ test/uninitialized-variable/uninitialized-local-memcpy-read.c
@@ -0,0 +1,11 @@
+int memcpy_reads_uninitialized_source(void)
+{
+    int src;
+    int dst = 0;
+    __builtin_memcpy(&dst, &src, sizeof(src));
+    return dst;
+}
+
+// at line 5, column 5
+// [!!] potential read of uninitialized local variable 'src'
+//      this load may execute before any definite initialization on all control-flow paths
