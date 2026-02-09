49aa131685b438cf265839ebe721ab314feebe5b
Hugo <hugo.payet@epitech.eu>
2026-02-09 02:41:21 +0100

    test(uninitialized): add basic uninitialized struct field read case

---
+++ test/uninitialized-variable/uninitialized-local-struct.c
@@ -0,0 +1,15 @@
+struct Pair
+{
+    int x;
+    int y;
+};
+
+int read_struct_field(void)
+{
+    struct Pair p;
+    return p.x;
+}
+
+// at line 10, column 14
+// [!!] potential read of uninitialized local variable 'p'
+//      this load may execute before any definite initialization on all control-flow paths
