75ee66e5321788b68c00f36abc8f0a9e042c1f1e
Hugo <hugo.payet@epitech.eu>
2026-02-09 02:41:21 +0100

    test(uninitialized): add switch-dependent initialization case

---
+++ test/uninitialized-variable/uninitialized-local-switch.c
@@ -0,0 +1,20 @@
+int read_switch(int tag)
+{
+    int value;
+    switch (tag)
+    {
+    case 0:
+        value = 10;
+        break;
+    case 1:
+        break;
+    default:
+        value = 20;
+        break;
+    }
+    return value;
+}
+
+// at line 15, column 12
+// [!!] potential read of uninitialized local variable 'value'
+//      this load may execute before any definite initialization on all control-flow paths
