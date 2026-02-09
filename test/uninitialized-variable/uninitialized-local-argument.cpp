42dde51123f927756d1b718918b201ae958721aa
Hugo <hugo.payet@epitech.eu>
2026-02-09 02:41:20 +0100

    test(uninitialized): add C++ argument handling without false positives on argc argv

---
+++ test/uninitialized-variable/uninitialized-local-argument.cpp
@@ -0,0 +1,22 @@
+#include <cstring>
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::string value;
+    uint16_t value16;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        const char* arg = argv[i];
+        if (std::strcmp(arg, "--help") == 0 || std::strcmp(arg, "-h") == 0)
+        {
+            return 1;
+        }
+    }
+    std::cout << value << std::endl;
+    return 0;
+}
+
+// not contains: potential read of uninitialized local variable 'argc.addr'
+// not contains: potential read of uninitialized local variable 'argv.addr'
