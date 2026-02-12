3c9a618d29a74b43520aeba378aca9e75e303e2a
Hugo <hugo.payet@epitech.eu>
2026-02-12 01:23:56 +0100

    test(recursion-c): assert location for infinite recursion diagnostics

--- test/recursion/c/infinite-recursion.c
+++ test/recursion/c/infinite-recursion.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+// at line 11, column 5
 // [!] recursive or mutually recursive function detected
+
+// at line 11, column 5
 // [!!!] unconditional self recursion detected (no base case)
 //     this will eventually overflow the stack at runtime
 void tutu(void)
