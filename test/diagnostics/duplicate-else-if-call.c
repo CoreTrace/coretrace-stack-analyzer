1049206db1429df80cf90447862ad90efef1b223
Hugo <hugo.payet@epitech.eu>
2026-02-12 01:23:57 +0100

    test(duplicate-if): add regression for duplicate else-if via helper call

---
+++ test/diagnostics/duplicate-else-if-call.c
@@ -0,0 +1,27 @@
+#include <string.h>
+
+static int same_flag(const char* arg)
+{
+    return strcmp(arg, "--stack-limit") == 0;
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+        return 0;
+
+    const char* arg = argv[1];
+    if (same_flag(arg))
+    {
+        return 1;
+    }
+    // at line 21, column 14
+    // [!] unreachable else-if branch: condition is equivalent to a previous 'if' condition
+    //     else branch implies previous condition is false
+    else if (same_flag(arg))
+    {
+        return 2;
+    }
+
+    return 0;
+}
