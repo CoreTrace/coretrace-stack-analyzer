f6428526fde9d11bd7d5889af2f185d2c7a3d6b8
Hugo <hugo.payet@epitech.eu>
2026-02-12 13:14:14 +0100

    test(multiple-storage): cover same-index multiple-store diagnostic

--- test/multiple-storage/same-storage.c
+++ test/multiple-storage/same-storage.c
@@ -15,3 +15,7 @@ int main(void)
     foo();
     return 0;
 }
+
+// at line 5, column 1
+// [!Info!] multiple stores to stack buffer 'buf' in this function (4 store instruction(s), 3 distinct index expression(s))
+// [!Info!] stores use different index expressions; verify indices are correct and non-overlapping
