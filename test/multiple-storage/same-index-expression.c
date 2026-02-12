9bfb47bd0f80f3aeaac0448dce9ac839f8c26e5d
Hugo <hugo.payet@epitech.eu>
2026-02-12 13:13:47 +0100

    test(multiple-storage): cover same-index multiple-store diagnostic

---
+++ test/multiple-storage/same-index-expression.c
@@ -0,0 +1,16 @@
+void same_index_store(void)
+{
+    char buf[10];
+    buf[2] = 'a';
+    buf[2] = 'b';
+}
+
+int main(void)
+{
+    same_index_store();
+    return 0;
+}
+
+// at line 3, column 1
+// [!Info!] multiple stores to stack buffer 'buf' in this function (2 store instruction(s), 1 distinct index expression(s))
+// [!Info!] all stores use the same index expression (possible redundant or unintended overwrite)
