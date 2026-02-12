4d15327b05e10c860b4fc2ec89a7db19d1bea3c0
Hugo <hugo.payet@epitech.eu>
2026-02-12 01:23:55 +0100

    feat(extern-example): load compile commands database before analysis

--- extern-project/src/main.cpp
+++ extern-project/src/main.cpp
@@ -2,6 +2,7 @@
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/IR/LLVMContext.h>
 #include <iostream>
+#include "analysis/CompileCommands.hpp"
 
 int main(int argc, char **argv)
 {
@@ -12,7 +13,17 @@ int main(int argc, char **argv)
     }
 
     std::string filename = argv[1];
+    std::string compile_file = argv[2];
+    std::string dbLoadError;
 
+    std::cout << compile_file << std::endl;
+    auto db = ctrace::stack::analysis::CompilationDatabase::loadFromFile(compile_file,
+                                                                        dbLoadError);
+    if (!db)
+    {
+        std::cerr << "Failed to load compilation database: " << dbLoadError << std::endl;
+        return 1;
+    }
     llvm::LLVMContext ctx;
     llvm::SMDiagnostic diag;
 
@@ -20,6 +31,8 @@ int main(int argc, char **argv)
     cfg.mode = ctrace::stack::AnalysisMode::IR;
     cfg.stackLimit = 8 * 1024 * 1024;
 
+    cfg.compilationDatabase = std::move(db);
+    // std::call_once(ctrace::stack::initializeLLVM, ctrace::stack::initializeLLVMFlag);
     auto res = ctrace::stack::analyzeFile(filename, cfg, ctx, diag);
 
     // Example: SARIF output to stdout
