180baa315d63a047eda4d6dd6bc98533a68d4305
Hugo <hugo.payet@epitech.eu>
2026-03-01 23:14:34 +0900

    test(fixtures): update expectations in format_problem.c

---
+++ test/coretrace-legacy/format_problem.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void print_input(char* input)
+{
+    printf(input);
+}
+
+int main()
+{
+    char user_input[100];
+    printf("Entrez une chaîne : ");
+    fgets(user_input, 100, stdin);
+    print_input(user_input);
+    return 0;
+}
+
+// at line 5, column 12
+// [ !!Warn ] non-literal format string may allow format injection
+
+// at line 10, column 1
+// [ !!Warn ] local variable 'user_input' is never initialized
