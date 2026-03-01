68b60f4cc0198c9f47ce21917bb03fda2004c16e
Hugo <hugo.payet@epitech.eu>
2026-03-01 23:16:48 +0900

    test(fixtures): add or update EmptyForStatement.cc

---
+++ test/coretrace-legacy/EmptyForStatement.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+        ; // Boucle vide : le point-virgule termine la boucle immédiatement.
+    {
+        printf("Hello\n");
+    }
+}
