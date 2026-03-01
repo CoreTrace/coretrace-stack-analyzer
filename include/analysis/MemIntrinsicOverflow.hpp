55e68bb5e671cd795a86d1b665bb6e2b346e932c
Hugo <hugo.payet@epitech.eu>
2026-03-01 23:16:46 +0900

    feat(analyzer): update MemIntrinsicOverflow.hpp

--- include/analysis/MemIntrinsicOverflow.hpp
+++ include/analysis/MemIntrinsicOverflow.hpp
@@ -20,13 +20,15 @@ namespace ctrace::stack::analysis
     {
         std::string funcName;
         std::string varName;
-        std::string intrinsicName; // "memcpy" / "memset" / "memmove"
+        std::string intrinsicName;
         StackSize destSizeBytes = 0;
         StackSize lengthBytes = 0;
+        bool hasExplicitLength = false;
         const llvm::Instruction* inst = nullptr;
     };
 
     std::vector<MemIntrinsicIssue>
     analyzeMemIntrinsicOverflows(llvm::Module& mod, const llvm::DataLayout& DL,
-                                 const std::function<bool(const llvm::Function&)>& shouldAnalyze);
+                                 const std::function<bool(const llvm::Function&)>& shouldAnalyze,
+                                 const std::string& bufferModelPath = "");
 } // namespace ctrace::stack::analysis
