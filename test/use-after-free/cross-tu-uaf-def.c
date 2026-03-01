1b4f90eb007569d493a8f15bb4f1f5f35d992042
Hugo <hugo.payet@epitech.eu>
2026-03-01 23:14:35 +0900

    test(inter-tu): add cross-tu regression fixture cross-tu-uaf-def.c

---
+++ test/use-after-free/cross-tu-uaf-def.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+void* acquire_handle(void)
+{
+    return malloc(32);
+}
+
+void release_handle(void* h)
+{
+    free(h);
+}
+
+void* acquire_handle_wrapper(void)
+{
+    return acquire_handle();
+}
+
+void release_handle_wrapper(void* h)
+{
+    release_handle(h);
+}
