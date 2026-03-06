caa78094ba490bd334d54f506bce491cc906e7c8
Hugo <hugo.payet@epitech.eu>
2026-03-07 03:45:35 +0900

    feat(smt): add pluggable solver strategy interface

---
+++ include/analysis/smt/ISolverStrategy.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "analysis/smt/ISmtBackend.hpp"
+
+#include <memory>
+#include <vector>
+
+namespace ctrace::stack::analysis::smt
+{
+    class ISolverStrategy
+    {
+      public:
+        virtual ~ISolverStrategy() = default;
+        virtual std::vector<SmtAnswer>
+        run(const SmtQuery& query, const std::vector<std::shared_ptr<ISmtBackend>>& backends) const = 0;
+    };
+} // namespace ctrace::stack::analysis::smt
+
