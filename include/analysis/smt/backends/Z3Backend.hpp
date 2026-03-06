554f371ab4abec08ff76d1231c7d681d2e943f05
Hugo <hugo.payet@epitech.eu>
2026-03-07 03:45:36 +0900

    feat(smt-z3): add z3 backend public interface

---
+++ include/analysis/smt/backends/Z3Backend.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "analysis/smt/ISmtBackend.hpp"
+
+namespace ctrace::stack::analysis::smt
+{
+    class Z3Backend final : public ISmtBackend
+    {
+      public:
+        Z3Backend() = default;
+
+        std::string name() const override;
+        SmtAnswer solve(const SmtQuery& query) const override;
+    };
+} // namespace ctrace::stack::analysis::smt
