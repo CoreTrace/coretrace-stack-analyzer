e24dd65bf3d0de1560e179ff7f508a89dc5ebbe5
Hugo <hugo.payet@epitech.eu>
2026-03-09 21:55:34 +0900

    test(analysis): update global-array-read-without-local-write.c

---
+++ test/uninitialized-variable/global-array-read-without-local-write.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int g_zero_cross_state[8];
+int g_zero_local_ok[4];
+
+static void external_write(void)
+{
+    g_zero_cross_state[0] = 42;
+}
+
+static int read_without_local_write(void)
+{
+    return g_zero_cross_state[0];
+}
+// at line 13, column 12
+// [ !!Warn ] potential read of global buffer 'g_zero_cross_state' before first write in this function
+//         ↳ no write to this buffer is observed in this function; value may come from static initialization or writes in other functions/TUs
+
+static int read_after_local_write(void)
+{
+    g_zero_local_ok[1] = 7;
+    return g_zero_local_ok[1];
+}
+// not contains: potential read of global buffer 'g_zero_local_ok' before first write in this function
+
+int main(void)
+{
+    external_write();
+    const int a = read_without_local_write();
+    const int b = read_after_local_write();
+    printf("%d %d\\n", a, b);
+    return 0;
+}
